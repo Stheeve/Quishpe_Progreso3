43b285579c84c4450a352fba0d84b726a30393c5
Stheeven Quishpe <stheeven.quishpe@udla.edu.ec>
2024-01-23 15:16:16 -0500

    Se uso la funcion main para poder inicializar las variables que se va a utilizar dentro del codigo propuesto

--- Progreso.c
+++ Progreso.c
@@ -9,3 +9,18 @@ struct alumno
     char materia[50];
     float nota;
 };
+int main(){
+
+    int opcion;
+    struct alumno alumnos[20];
+    int num;
+    int estudiantes=0;
+    FILE*archivo;
+
+    printf ("Coloque le numero de alumnos que desee ingresar\n");
+    printf ("Se puede ingresar maximo 20 estudiantes\n");
+    scanf("%d",&num);
+
+
+}
+
