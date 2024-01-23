fea5919745ebba5ea416e58ab6af8070ddb3a448
Stheeven Quishpe <stheeven.quishpe@udla.edu.ec>
2024-01-23 15:30:22 -0500

    Se agregaron comentarios para entender de emjor manera la construccion del programa

--- Progreso.c
+++ Progreso.c
@@ -1,6 +1,7 @@
+//librerias requeridas para el programa
 #include <stdio.h>
 #include <string.h>
-
+//estructura utilizada para el programa 
 struct alumno
 {
     int matricula;
@@ -11,9 +12,9 @@ struct alumno
 };
 int main(){
 
-    int opcion;
-    struct alumno alumnos[20];
-    int num;
+    int opcion;//variable para el menu
+    struct alumno alumnos[20];//variable para que se vayan guardando datos de los estudiantes
+    int num;//variable para saber el numero de estudiantes ingresados
     int estudiantes=0;
     FILE*archivo;
 
@@ -28,17 +29,19 @@ int main(){
         printf("Opcion 2. Mostrar alumnos \n");
         printf("Opcion 3. Salir\n");
         scanf("%d", &opcion);
+        //diferentes opciones que da el programa al usuario
 
-        switch (opcion)
+        switch (opcion)//switch usado como menu
         {
-        case 1:
-            for (int i = 0; i < num; i++)
+        case 1://case en el cual se pedira los difetentes datos del estudiante
+            for (int i = 0; i < num; i++)//for para ingresar los estudiantes solicitados
             {
                  
                 printf("\nDatos del estudiante %d:\n", estudiantes + 1);
 
                 printf("Ingrese la matricula: ");
                 fflush(stdin);
+                //limpia el espacio de la memoria
                 scanf("%d", &alumnos[estudiantes].matricula);
 
                 printf("Ingrese el nombre: ");
@@ -61,18 +64,18 @@ int main(){
                 printf("Estudiante ingresado exitosamente.\n");
 
 
-                archivo = fopen("alumnos.txt", "w");
+                archivo = fopen("alumnos.txt", "w");//aqui se abre el archivo y se va modificando metiante se ingrese un nuevo alumno en un archivo llamado alumnos.txt
                 for (int i = 0; i < estudiantes; i++)
                 {
                     fprintf(archivo, "\nEstudiante %d:\t", i + 1);
+                    fprintf(archivo, "%d\t", alumnos[i].matricula);
                     fprintf(archivo, "%s\t", alumnos[i].nombre);
                     fprintf(archivo, "%s\t", alumnos[i].direccion);
                     fprintf(archivo, "%s\t", alumnos[i].materia);
-                    fprintf(archivo, "%d\t", alumnos[i].matricula);
                     fprintf(archivo, "%f\t", alumnos[i].nota);
                 }
 
-                fclose(archivo); 
+                fclose(archivo); //se cierra el archivo
                 printf("Datos guardados en el archivo 'alumnos.txt'.\n");
 
                 
@@ -81,20 +84,20 @@ int main(){
             break;
         
     
-        case 2:
+        case 2://case usado para imprimir en pantalla a los alumnos ingresados sin necesidad de abrir el archivo alumnos.txt
         for (int i = 0; i < estudiantes; i++)
                 {
                     printf("\nEstudiante %d:\t", i + 1);
+                    printf("%d\t", alumnos[i].matricula);
                     printf("%s\t", alumnos[i].nombre);
                     printf("%s\t", alumnos[i].direccion);
                     printf("%s\t", alumnos[i].materia);
-                    printf("%d\t", alumnos[i].matricula);
                     printf("%f\t\n", alumnos[i].nota);
                 }
 
         }
 
-    } while (opcion != 3);
+    } while (opcion != 3);//while para cerrar el programa
     
     
 }
